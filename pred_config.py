0047e2b6a9bf3e8b13ec31fbb8b14ac72d8a585c
Zabiullahkhan <53210912+Zabiullahkhan@users.noreply.github.com>
2024-05-30 12:37:58 +0530

    Create pred_config.py

---
+++ pred_config.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 18 15:39:09 2023
+
+@author: ZA40142720
+"""
+# =============================================================================
+# class TrainingConfig:
+#     def __init__(self):
+#         # Add other configuration parameters as needed
+#         self.LEARNING_RATE = 5e-5  # Adjust the learning rate accordingly
+#         self.NUM_TRAIN_EPOCHS = 2
+#         self.PER_DEVICE_TRAIN_BATCH_SIZE = 16
+#         self.PER_DEVICE_EVAL_BATCH_SIZE = 32
+#         self.LOAD_BEST_MODEL_AT_END = True
+#         self.WARMUP_STEPS = 500
+#         self.WEIGHT_DECAY = 1e-5
+#         self.LOGGING_DIR = './logs'
+#         self.EVALUATION_STRATEGY = "steps"
+#         self.EVAL_STEPS = 500
+#         self.LOGGING_STEPS = 500
+#         self.OUTPUT_DIR = './model'
+# # config.py
+# 
+# =============================================================================
+DATA_PATH = "./prediction_dataset"
+MODEL_PATH = "/home/wipro/NLP_RnD/PB_EC/PRODUCT/OUTPUT_DistillBERT_15_11_2023"
+TOKENIZER_PATH = "/home/wipro/NLP_RnD/PB_EC/PRODUCT/OUTPUT_DistillBERT_15_11_2023"
+PICKLE_FILE = "/home/wipro/NLP_RnD/PB_EC/PRODUCT/OUTPUT_DistillBERT_15_11_2023/Nov_15_ids2label.pickle"
+
+
